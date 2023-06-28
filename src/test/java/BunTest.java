5c91b2a0fe8ab351ccee1eba915ad4dbf8ba6dcc
Anna Nazmeeva <lightann93@mail.ru>
2023-06-28 21:27:31 +0300

    add Parameters

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,18 +1,37 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
-    Bun bun = new Bun("black bun", 100);
+    private final Bun bun;
+
+    public BunTest(Bun bun) {
+        this.bun = bun;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+                {new Bun("black bun", 100)},
+                {new Bun("white bun", 200)},
+                {new Bun("white bun", 10)},
+        });
+    }
 
     @Test
     public void getNameTest() {
-        assertEquals("black bun", bun.getName());
+        assertEquals(bun.getName(), bun.getName());
     }
 
     @Test
     public void getPriseTest() {
-        assertEquals(100, bun.getPrice(), 0.1);
+        assertEquals(bun.getPrice(), bun.getPrice(), 0.1);
     }
 }
