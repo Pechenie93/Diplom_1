5f23fa4da1319fecc80eab80bebfc65e6bf4310c
Anna Nazmeeva <lightann93@mail.ru>
2023-07-10 16:56:38 +0300

    add increasing test data

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -19,11 +19,15 @@ public class BunTest {
     @Parameterized.Parameters
     public static Collection<Object[]> testData() {
         return Arrays.asList(new Object[][]{
-                {new Bun("black bun", 100)},
-                {new Bun("white bun", 200)},
-                {new Bun("white bun", 10)},
-                {new Bun("white-black bun", 1)},
-                {new Bun("white bun", 0)},
+                {new Bun("black bun", 100)},             // Valid data - black bun with price 100
+                {new Bun("white bun", 200)},             // Valid data - white bun with price 200
+                {new Bun("white bun", 10)},              // Valid data - white bun with price 10
+                {new Bun("white-black bun", 1)},         // Valid data - white-black bun with price 1
+                {new Bun("white bun", 0)},               // Valid data - white bun with price 0
+                {new Bun(null, 100)},                     // Invalid data - null name with price 100
+                {new Bun("black bun", -50)},              // Invalid data - black bun with negative price
+                {new Bun("", 200)},                       // Invalid data - empty name with price 200
+                {new Bun("white bun", 1000000)},          // Invalid data - white bun with a very high price
         });
     }
 
